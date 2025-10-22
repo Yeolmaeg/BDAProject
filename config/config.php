56ad1ec3f02af3688512048b72489033251425ee
Yeolmaeg <ptgson@gmail.com>
2025-10-23 04:54:51 +0900

    서버 기본값, 로컬 오버라이드

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+$DB_HOST = 'localhost';
+$DB_NAME = 'team04';
+$DB_USER = 'team04';
+$DB_PASS = 'team04';
+$DB_PORT = 3306;
+
+// 로컬 작업시 덮어쓸 분만 쓰시면 됩니다
+$local = __DIR__ . '/config.local.php';
+if (file_exists($local)) require $local; 
+
+define('DB_HOST', $DB_HOST);
+define('DB_NAME', $DB_NAME);
+define('DB_USER', $DB_USER);
+define('DB_PASS', $DB_PASS);
+define('DB_PORT', $DB_PORT);
+

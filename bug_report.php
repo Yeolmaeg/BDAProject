f6e6439ddfea876e9e634ec87bbd765ec3e9b4c0
27marchsa-debug <27marchsa@ewhain.net>
2025-11-19 15:55:02 +0900

    Update alert message to English for login check

--- bug_report.php
+++ bug_report.php
@@ -13,7 +13,7 @@ if (session_status() === PHP_SESSION_NONE) {
 // 로그인 체크 로직
 if (!isset($_SESSION['user_id'])) {
     echo "<script>
-        alert('로그인이 필요한 서비스입니다.');
+        alert('You need to log in before using this service.');
         location.href = 'login.php'; 
     </script>";
     exit;
